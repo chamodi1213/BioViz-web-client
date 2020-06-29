fe7d20cfe7fe97c57c7f88f2688099f23273ab99
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-29 08:49:14 +0000

    Deploy budvinchathura/BioViz-web-client to github.com/budvinchathura/BioViz-web-client.git:gh-pages

---
+++ precache-manifest.20f8ea1b0a8c7e38065540fc034b79eb.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5496cf7ee238abb7a0bcfffb690105",
+    "url": "/BioViz-web-client/index.html"
+  },
+  {
+    "revision": "cc1a94eddaedd39bc4f2",
+    "url": "/BioViz-web-client/static/css/main.a3537bcf.chunk.css"
+  },
+  {
+    "revision": "369cd0aa47b99f09c034",
+    "url": "/BioViz-web-client/static/js/2.54799655.chunk.js"
+  },
+  {
+    "revision": "4f3ee35d4ae1279a07ca6ba39f640448",
+    "url": "/BioViz-web-client/static/js/2.54799655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1a94eddaedd39bc4f2",
+    "url": "/BioViz-web-client/static/js/main.d5b05db9.chunk.js"
+  },
+  {
+    "revision": "71c0809e4a65b0e9bab2",
+    "url": "/BioViz-web-client/static/js/runtime-main.d97b5dd1.js"
+  },
+  {
+    "revision": "eff3baf80c34d3f9455d22f0d4f2e7ea",
+    "url": "/BioViz-web-client/static/media/1.eff3baf8.jpg"
+  },
+  {
+    "revision": "a44f7dee3d1590eb946b1a9db0e8cc40",
+    "url": "/BioViz-web-client/static/media/2.a44f7dee.jpg"
+  },
+  {
+    "revision": "7d29872643dc1b29353691663384810e",
+    "url": "/BioViz-web-client/static/media/3.7d298726.png"
+  },
+  {
+    "revision": "f1cd5369c62534ab4724bfdf749db601",
+    "url": "/BioViz-web-client/static/media/MSA.f1cd5369.png"
+  },
+  {
+    "revision": "701c38d52f54508bcf0604c7367e635a",
+    "url": "/BioViz-web-client/static/media/dna.701c38d5.svg"
+  },
+  {
+    "revision": "b26b42c0999c9fe17a2f05181ab01af8",
+    "url": "/BioViz-web-client/static/media/input1.b26b42c0.jpg"
+  },
+  {
+    "revision": "1e3a4e886796c9f7311cc9726c7f2dd1",
+    "url": "/BioViz-web-client/static/media/input3.1e3a4e88.jpg"
+  },
+  {
+    "revision": "93bb85223b3125d370de2db48d4f728c",
+    "url": "/BioViz-web-client/static/media/input4.93bb8522.jpg"
+  },
+  {
+    "revision": "78ccc254aa806328b31e4d3ed14f3ed2",
+    "url": "/BioViz-web-client/static/media/knoldge.78ccc254.jpg"
+  },
+  {
+    "revision": "26d4019477ed2062e1e3e0ad53a7dcc2",
+    "url": "/BioViz-web-client/static/media/pw.26d40194.png"
+  },
+  {
+    "revision": "201c7b06a0347ac2ef69470d206c5961",
+    "url": "/BioViz-web-client/static/media/score2.201c7b06.jpg"
+  },
+  {
+    "revision": "463bf52ffefacdb6ae650f04c1f8fb8c",
+    "url": "/BioViz-web-client/static/media/select2.463bf52f.jpg"
+  },
+  {
+    "revision": "5ecf8d7e9a517a214be6cf142901e06a",
+    "url": "/BioViz-web-client/static/media/select3.5ecf8d7e.jpg"
+  },
+  {
+    "revision": "561b0ec6f4c6aca251782ba5a48b901d",
+    "url": "/BioViz-web-client/static/media/variable1.561b0ec6.jpg"
+  },
+  {
+    "revision": "fccd97a68ffac0d54a6dcfb0dccb35f1",
+    "url": "/BioViz-web-client/static/media/variable4.fccd97a6.png"
+  }
+]);
